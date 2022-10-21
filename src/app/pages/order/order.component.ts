410842122bb296edcec5e63c55d52e6d6b808bad
rizbons <rizbo.ns.bk@gmail.com>
2022-10-21 10:15:26 +0200

    style-tabs

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
 })
 export class OrderComponent implements OnInit {
+  tabs: string[] = ['All', 'Status', 'Customer', 'Product', 'Ship from'];
+  selectedTab: string = this.tabs[0];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  selectTab(tab: string) {
+    this.selectedTab = tab;
   }
-
 }
